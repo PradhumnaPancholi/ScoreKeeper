f69ad7ee1bfd592c66801e95504dd501bc0097e3
Pradhumna Pancholi <29339287+PradhumnaPancholi@users.noreply.github.com>
2018-04-17 19:11:56 -0400

    Update ScoreKeeper.js

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -4,6 +4,11 @@ alert("Powered by JavaScript");
 var p1Button = document.querySelector("#p1");
 //for selecting  player 2 (button)//
 var p2Button = document.querySelector("#p2");
+//for input of winning number//
+var p = document.querySelector("p");
+var numInput = document.querySelector("input");
+//for displaying winning number//
+var winningScoreDisplay = document.querySelector("p span");
 //for reset button//
 var resetButton = document.querySelector("#reset");
 var p1Score = 0;
@@ -12,16 +17,26 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var gameOver = false;
 var winningScore = 7;
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
 
 //for p1 button functionality//
 p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
-            console.log("p1 Won");
+            p1Display.classList.add("winner");
             gameOver = true;
         }   
-        p1Display.textContent = p1Score;    
+        p1Display.textContent = p1Score; 
+ 
     }
 
 });
@@ -30,13 +45,25 @@ p1Button.addEventListener("click", function(){
 p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
+        console.log(p2Score, winningScore)
         if(p2Score === winningScore){
-            console.log("p1 WOn");
+            p2Display.classList.add("winner");
             gameOver = true;
         }
     }
     p2Display.textContent = p2Score;
+
 });
 
 //for reset button functionality//
-resetButton
+resetButton.addEventListener("click", function(){
+    reset();
+});
+
+//for winning num input//
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
+});
